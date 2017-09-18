b6294781020561567633db955c26c3d0c12e197e
dragongrox <31999215+dragongrox@users.noreply.github.com>
2017-09-18 13:07:03 +0200

    hola

--- app/src/main/java/com/example/drago/myapplication/MainActivity.java
+++ app/src/main/java/com/example/drago/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
-    private Button btn_pulsa;
+    private Button btn_borrar_todo,btn_borrar_caracter,btn_9,btn_8,btn_7,btn_6,btn_5,btn_4,btn_3,btn_2,btn_1,btn_0,btn_x,btn_menos,btn_mas,btn_bividir,btn_coma,btn_igual;
     private TextView tv_contador;
     private ImageView Img_eevee;
     private int num=1;
@@ -22,22 +22,13 @@ public class MainActivity extends AppCompatActivity{
 
         inflar();
 
-        btn_pulsa.setOnClickListener(new View.OnClickListener() {
+        btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (tv_contador.getText().toString().equals("Contador"))
+                if (tv_contador.getText().toString().equals("0"))
                     tv_contador.setText("1");
-                else {
+                else
                     tv_contador.setText((Integer.parseInt(tv_contador.getText().toString().trim()) + 1) + "");
-
-                }
-            }
-        });
-
-        btn_pulsa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
             }
         });
 
@@ -45,8 +36,20 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private void inflar() {
-        btn_pulsa = (Button)findViewById(R.id.bt_pulsa);
+        btn_0 = (Button)findViewById(R.id.btn_0);
+        btn_1 = (Button)findViewById(R.id.btn_1);
+        btn_2 = (Button)findViewById(R.id.btn_2);
+        btn_3 = (Button)findViewById(R.id.btn_3);
+        btn_4 = (Button)findViewById(R.id.btn_4);
+        btn_5 = (Button)findViewById(R.id.btn_5);
+        btn_6 = (Button)findViewById(R.id.btn_6);
+        btn_6 = (Button)findViewById(R.id.btn_6);
+        btn_7 = (Button)findViewById(R.id.btn_7);
+        btn_8 = (Button)findViewById(R.id.btn_8);
+        btn_9 = (Button)findViewById(R.id.btn_9);
+        btn_borrar_todo = (Button)findViewById(R.id.btn_borrar_todo);
+        btn_borrar_caracter = (Button)findViewById(R.id.btn_borrar_caracter);
         tv_contador = (TextView)findViewById(R.id.txt_contador);
-        Img_eevee = (ImageView) findViewById(R.id.Img_eevee);
+        btn_x = (Button)findViewById(R.id.btn_x)
     }
 }
